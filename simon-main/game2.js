33fc46637fe66389e0ae21820b4a636e3fe34a9d
Nadav Nuni <nuni@kovrr.com>
2022-07-09 23:00:54 +0300

    added fades function and sounds

--- simon-main/game2.js
+++ simon-main/game2.js
@@ -9,43 +9,61 @@ document.addEventListener("keypress", addNewValue, {
 function addNewValue() {
   const randomStartNumber = Math.floor(Math.random() * 4);
   currentGame.push(randomStartNumber);
-  $("#" + randomStartNumber)
-    .fadeOut(120)
-    .fadeIn(120);
+  fades(randomStartNumber, true);
   clientTurnClicks();
 }
 
 // get cilck value from the user and check if it match to the numbers in the arry "currentGame"
 function clientTurnClicks() {
-  let ClickCounter = 0; //currnt turn counter 
-  $(".btn").on("click", function buttonClickHandler(event) { 
+  let ClickCounter = 0; //currnt turn counter
+  $(".btn").on("click", function buttonClickHandler(event) {
     const clientClickBtn = event.target.id; //get the user's pressed element id
-    if (clientClickBtn == currentGame[ClickCounter]) { //if the user's click is correct
-      $(this).fadeOut(120).fadeIn(120);
+    if (clientClickBtn == currentGame[ClickCounter]) {
+      //if the user's click is correct
+      fades(clientClickBtn, false);
       ClickCounter = ClickCounter + 1; // increase the counter so we can check the next array value
-      if (ClickCounter >= currentGame.length) { // compare the user's click vs the array length
+      if (ClickCounter >= currentGame.length) {
+        // compare the user's click vs the array length
         $(".btn").off("click"); // block listen to user clicks
         showGamePattern(ClickCounter); //**run the "showGamePattern" patten with the value of the current level
       } // else do nothing => wait for the next user click
-    } else { //if the user click value is wrong end the game
+    } else {
+      //if the user click value is wrong end the game
       alert("Game Over");
-      location.reload(); //refresh the page
+      location.reload();
     }
   });
 }
 
 //run all the the value in the "currentGame" array
-function showGamePattern(ClickCounter) { //use the current level as input
-  if (ClickCounter > 0) { //** as long as the counter grater then 0 run the next value */
+function showGamePattern(ClickCounter) {
+  //use the current level as input
+  if (ClickCounter > 0) {
+    //** as long as the counter grater then 0 run the next value */
     setTimeout(function () {
-      let x = currentGame.length-ClickCounter;
-      $("#" + currentGame[x])
-        .fadeOut(120)
-        .fadeIn(120);
-        ClickCounter = ClickCounter - 1;
-      showGamePattern(ClickCounter);
+      const x = currentGame.length - ClickCounter;
+      fades(currentGame[x], true);
+      showGamePattern(ClickCounter - 1);
     }, 1000);
-  } else { //if the counter is 0 run the function that add new number to the array
+  } else {
+    //if the counter is 0 run the function that add new number to the array
     setTimeout(addNewValue, 1000);
   }
+}
+
+function fades(divId, shouldFade) {
+  const soundColorUser = new Audio("sounds/" + divId + ".mp3");
+  soundColorUser.play();
+
+  if (shouldFade) {
+    $("#" + divId)
+      .fadeOut(120)
+      .fadeIn(120);
+  } else {
+    $("#" + divId).addClass("pressed");
+    setTimeout(function () {
+      $("#" + divId).removeClass("pressed");
+    }, 120);
+  }
+ 
 }
