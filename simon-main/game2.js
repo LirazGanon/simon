41c70197eceee47bdff31e93983d4f4833f15c80
לירז גנון <lirazganon@gmail.com>
2022-07-11 22:10:36 +0300

    added record and more responsive

--- simon-main/game2.js
+++ simon-main/game2.js
@@ -1,21 +1,23 @@
 const currentGame = []; // An Array that hold the random game numbers
 let width = screen.width;
+let record = localStorage.getItem("1");
+if (record) {
+  $("h2").text("HIGH SCORE: level " + record);
+}
 
-if (width > 700){
+//mobile responsive
+if (width > 700) {
   document.addEventListener("keypress", addNewValue, {
     once: true,
   });
-} else{
-    $("h1").text("Press here to start");
-   document.querySelector("h1").addEventListener("click", addNewValue, {
+} else {
+  $("h1").text("Touch here to start");
+  document.querySelector("h1").addEventListener("click", addNewValue, {
     once: true,
   });
 }
 
-
-
-
-// add new random number to the array, flash the match element and than run function "clientTurnClicks"
+// add new random number to currentGame
 function addNewValue() {
   const randomStartNumber = Math.floor(Math.random() * 4);
   currentGame.push(randomStartNumber);
@@ -34,18 +36,20 @@ function clientTurnClicks() {
       ClickCounter = ClickCounter + 1;
       if (ClickCounter >= currentGame.length) {
         $(".btn").off("click");
-        setTimeout(function(){
+        setTimeout(function() {
           showGamePattern(currentGame.length);
-        },200);
+        }, 200);
       }
-    } else {
+    } else { //if wrong Game Over
       const soundGameOver = new Audio("sounds/game-over.mp3");
       soundGameOver.play();
       $("body").addClass("game-over");
-      setTimeout (function(){
-      alert("Game Over");
-      location.reload();
-    },1000);
+      record = currentGame.length - 1;
+      localStorage.setItem("1", record);
+      setTimeout(function() {
+        alert("Game Over");
+        location.reload();
+      }, 1000);
     }
   });
 }
@@ -54,7 +58,7 @@ function clientTurnClicks() {
 function showGamePattern(loopCounter) {
   $("h1").text("Level " + (currentGame.length + 1));
   if (loopCounter > 0) {
-    setTimeout(function () {
+    setTimeout(function() {
       const x = currentGame.length - loopCounter;
       fades(currentGame[x], true);
       showGamePattern(loopCounter - 1);
@@ -64,19 +68,18 @@ function showGamePattern(loopCounter) {
   }
 }
 
+//fade or flash the button
 function fades(divId, shouldFade) {
   const soundColorUser = new Audio("sounds/" + divId + ".mp3");
   soundColorUser.play();
-
   if (shouldFade) {
     $("#" + divId)
       .fadeOut(120)
       .fadeIn(120);
   } else {
     $("#" + divId).addClass("pressed");
-    setTimeout(function () {
+    setTimeout(function() {
       $("#" + divId).removeClass("pressed");
     }, 120);
   }
-
 }
